bd354be9e6e2db082b8513a91a57b3a4ba1a45c4
sarakho <122581761+sarakho@users.noreply.github.com>
2023-04-13 18:39:50 -0400

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -34,3 +34,50 @@ function removeClass(element, name) {
     }
     element.className = arr1.join(" ")
 }
+
+
+
+
+
+
+
+/* another version of filterting*/
+
+// const btns = document.querySelectorAll('.buttons');
+// const imgs = document.querySelectorAll('.card');
+
+// for(let i = 1; i < btns.legnth; i++) {
+//     btns[i].addEventListener('click', filterImg);
+// }
+
+// function setActiveBtn(e) {
+//     btns.forEach(btn => {
+//         btn.classList.remove('btn-clicked');
+//     });
+//     e.target.classList.add('btn-clicked');
+// }
+
+// function filterImg(e) {
+//     setActiveBtn(e);
+
+//     imgs.forEach(img => {
+//         img.classList.remove('img-shrink');
+//         img.classList.add('img-expand');
+
+//         const imgType = parseInt(img.dataset.img);
+//         const btnType = parseInt(e.target.dataset.btn);
+
+//         if(imgType !== btnType) {
+//             img.classList.remove('img-expand');
+//             img.classList.add('img-shrink');
+//         }
+//     });
+// }
+
+// btns[0].addEventListener('click', (e) => {
+//     setActiveBtn(e);
+//     imgs.forEach(img => {
+//         img.classList.remove('img-shrink');
+//         img.classList.add('img-expanded');
+//     });
+// })
