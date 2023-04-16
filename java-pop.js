9b45950800f2fca16794f651360f6684258eabdf
sarakho <122581761+sarakho@users.noreply.github.com>
2023-04-16 19:37:19 -0400

    Add files via upload

---
+++ java-pop.js
@@ -0,0 +1,75 @@
+// let atag = document.querySelectorAll("#link-pop");
+// let btag = document.querySelector("#pop");
+
+// let clickEvent = () => {
+//     btag.style.display = "block";
+// }
+
+// atag.forEach((item) => {
+//     item.addEventListener('click', clickEvent)
+// });
+
+// const ctag = document.querySelector("#pop");
+//     document.querySelector("#close-btn").addEventListener("click", () => {
+//         ctag.style.display = "none";
+// });
+
+
+function openPop1() {
+    document.querySelector("#a1").style.display = "block";
+}
+function closePop1() {
+    document.querySelector("#a1").style.display = "none";
+}
+
+
+
+
+function openPop2() {
+    document.querySelector("#a2").style.display = "block";
+}
+function closePop2() {
+    document.querySelector("#a2").style.display = "none";
+}
+
+
+
+
+function openPop3() {
+    document.querySelector("#a3").style.display = "block";
+}
+function closePop3() {
+    document.querySelector("#a3").style.display = "none";
+}
+
+
+
+
+function openPop4() {
+    document.querySelector("#a4").style.display = "block";
+}
+function closePop4() {
+    document.querySelector("#a4").style.display = "none";
+}
+
+
+
+
+
+function openPop5() {
+    document.querySelector("#a5").style.display = "block";
+}
+function closePop5() {
+    document.querySelector("#a5").style.display = "none";
+}
+
+
+
+
+
+function openPop6() {
+    document.querySelector("#a6").style.display = "block";
+}
+function closePop6() {
+    document.querySelector("#a6").style.display = "none";
+}
